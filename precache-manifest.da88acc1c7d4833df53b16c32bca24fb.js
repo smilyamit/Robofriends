4f1d06f41007a2c9a9319062507926b5bfd71e0b
Amit Tiwari <smilyamitt@gmail.com>
2019-12-04 21:23:31 +0100

    Updates

---
+++ precache-manifest.da88acc1c7d4833df53b16c32bca24fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc456baf16bf6ed550c1586f1c9d0651",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "976285af46a16683aefd",
+    "url": "/Robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "54c52853bd1bc0b66619",
+    "url": "/Robofriends/static/css/main.50754ed7.chunk.css"
+  },
+  {
+    "revision": "976285af46a16683aefd",
+    "url": "/Robofriends/static/js/2.e0ce671b.chunk.js"
+  },
+  {
+    "revision": "54c52853bd1bc0b66619",
+    "url": "/Robofriends/static/js/main.0d5296fe.chunk.js"
+  },
+  {
+    "revision": "3270d165655fe49ea0ac",
+    "url": "/Robofriends/static/js/runtime-main.b661ce97.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
